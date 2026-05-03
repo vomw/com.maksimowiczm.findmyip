de165299d01b375d689179c1cf46ae43b4431eca
vomw <22770640+vomw@users.noreply.github.com>
2026-05-03 21:20:08 +0800

    update IpifyConfig.kt

--- com.maksimowiczm.findmyip_15_src.tar.gz/app/src/main/java/com/maksimowiczm/findmyip/infrastructure/IpifyConfig.kt
+++ com.maksimowiczm.findmyip_15_src.tar.gz/app/src/main/java/com/maksimowiczm/findmyip/infrastructure/IpifyConfig.kt
@@ -1,6 +1,6 @@
 package com.maksimowiczm.findmyip.infrastructure
 
 object IpifyConfig {
-    const val IPV4 = "https://ipv4.myip.wtf/text"
-    const val IPV6 = "https://ipv6.myip.wtf/text"
+    const val IPV4 = "https://ipv4.seeip.org/"
+    const val IPV6 = "https://ipv6.seeip.org/"
 }
